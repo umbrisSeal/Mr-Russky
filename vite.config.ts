9631d50cb9b8752a9b669c9412eb28adccbd9f0d
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-21 23:19:01 -0600

    Updated vite.config to generate build for github pages.

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: '/Mr-Russky/',
+    build: {
+        outDir: 'docs'  // For github pages.
+    }
 })
