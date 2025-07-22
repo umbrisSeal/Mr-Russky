54cec22825c0d5e5f901755f8d7287d94defbcce
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-22 00:21:13 -0600

    Added react router structure to app.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router'
+import { router } from './routes/router.tsx'
 
 import './assets/styles/global/tailwind.css'
 import './assets/styles/global/normalize.css'
 import './assets/styles/global/global.css'
 
 
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>,
 );
