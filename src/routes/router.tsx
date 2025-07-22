2f1453dbd143babf442710ace2ca0327c39b1edd
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-22 10:08:36 -0600

    Updated router to use Vite mode.

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,12 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
-
+//import HomePage from "./homePage";
 
 
 export const router = createBrowserRouter([
-    { path: '/', element: <App /> },
-    { path: '*', element: <App /> }     // For 404 error.
-]);
+    { path: '/home', element: <App /> },
+    { path: '*', element: <App /> }                         // For 404 error.
+
+], {
+    basename: import.meta.env.MODE === 'production' ? '/Mr-Russky/' : '/'
+});
