2906eca63e19efe4134d48758ead9a0eb719685f
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-22 12:58:15 -0600

    Updated HomePage to use i18n messages.

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,11 +1,14 @@
 //import React from 'react'
 import { Box } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 import appLogo from '../assets/img/logo.svg';
 import PrimaryOptionButton from '../components/buttons/PrimaryOptionButton';
 import SecondaryOptionButton from '../components/buttons/SecondaryOptionButton';
 
 function HomePage() {
+    const { t } = useTranslation();
+
     return (
         <Box className='min-h-screen flex flex-col items-center justify-center space-y-8'>
             <img
@@ -15,18 +18,18 @@ function HomePage() {
             />
 
             <Box className='text-center'>
-                <h1 className='text-3xl font-bold text-accent'> ¡Bienvenido a Mr. Russky! </h1>
-                <p className='text-accent'> Practica y aprende nuevo vocabulario ruso. </p>
+                <h1 className='text-3xl font-bold text-accent'> {t('pages.homePage.welcomeMessage')} </h1>
+                <p className='text-accent'> {t('pages.homePage.title')} </p>
             </Box>
 
             <Box className='flex flex-col gap-3 items-center'>
-                <PrimaryOptionButton buttonMessage='Practica Personalizada' />
-                <SecondaryOptionButton buttonMessage='Top 500 Sustantivos' />
-                <SecondaryOptionButton buttonMessage='Top 500 Verbos' />
-                <SecondaryOptionButton buttonMessage='Top 1,000 Palabras' />
-                <SecondaryOptionButton buttonMessage='Cantidad de Vocabulario' />
+                <PrimaryOptionButton buttonMessage={t('components.buttons.options.customLesson')} />
+                <SecondaryOptionButton buttonMessage={t('components.buttons.options.topNouns')} />
+                <SecondaryOptionButton buttonMessage={t('components.buttons.options.topVerbs')} />
+                <SecondaryOptionButton buttonMessage={t('components.buttons.options.topWords')} />
+                <SecondaryOptionButton buttonMessage={t('components.buttons.options.vocabularyDB')} />
             </Box>
-            
+
         </Box>
     );
 }
