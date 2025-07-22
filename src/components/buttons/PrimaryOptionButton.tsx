73e0c932441be49a87308661eab681af09baa4ff
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-22 11:46:10 -0600

    Updated PrimaryOptionButton component.
     - Added border to match other OptionButton components.
     - Reduced paddingY from 2 to 1.5.

--- src/components/buttons/PrimaryOptionButton.tsx
+++ src/components/buttons/PrimaryOptionButton.tsx
@@ -3,14 +3,14 @@ import { Box, ButtonBase } from '@mui/material';
 import type { ButtonProps } from './buttonTypes';
 
 function PrimaryOptionButton({ buttonMessage = "Message not defined.", onClickFn = () => console.log("OnClick function not defined.") } : ButtonProps) {
-
+    
     return (
-        <Box className='w-full bg-secondary rounded-xl shadow-md/20 overflow-hidden'>
+        <Box className='w-full bg-secondary rounded-xl shadow-md/20 border-2 border-secondary overflow-hidden'>
             <ButtonBase
                 onClick={onClickFn}
                 sx={{
                     width: '100%',
-                    paddingY: 2,
+                    paddingY: 1.5,
                     paddingX: 3,
                     textAlign: 'center',
                     //transition: 'background-color 0.3s ease',
