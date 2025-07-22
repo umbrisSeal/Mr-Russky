0eee7b2a97a489a5cff98485f4e88fb0fa74425d
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-21 21:30:55 -0600

    Added initial noun list.

---
+++ src/data/words/inflected/nouns/initialNouns.ts
@@ -0,0 +1,42 @@
+
+
+// Initial noun words, only for example.
+
+import type { Noun } from "./noun";
+
+
+export const exampleNouns: Noun[] = [
+    {
+        id: "кошка",
+        gender: "female",
+        translation: {
+            es: "gata",
+            en: "cat"
+        },
+        cases: {
+            singular: {
+                nominative: "кошка",
+                genitive: "",
+                dative: "",
+                accusative: "",
+                instrumental: "",
+                prepositional: ""
+            },
+            plural: {
+                nominative: "кошки",
+                genitive: "",
+                dative: "",
+                accusative: "",
+                instrumental: "",
+                prepositional: ""
+            }
+        },
+        imageUrl: "",
+        difficulty: 1,
+        example: {
+            ru: "",
+            es: "",
+            en: ""
+        }
+    }
+];
