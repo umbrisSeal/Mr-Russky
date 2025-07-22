58f7a5ce4d92b03d586e3edce5c62f1e09cbb290
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-22 16:01:44 -0600

    Implemented TagTitle component in WordDatabasePage.

--- src/routes/WordDatabasePage.tsx
+++ src/routes/WordDatabasePage.tsx
@@ -1,8 +1,31 @@
 //import React from 'react'
+import { Box } from "@mui/material";
+import PageMotionWrapper from "../components/layout/PageMotionWrapper";
+import TagTitle from "../components/tags/TagTitle";
 
 function WordDatabasePage() {
     return (
-        <p> Show word count here by category. </p>
+        <PageMotionWrapper>
+            <Box className=''>
+                { /* Title and Tags */ }
+                <Box className='flex flex-col items-center gap-6 p-6'>
+                    <h1 className="font-bold text-accent text-3xl text-center">
+                        Cantidad de Vocabulario por Categoria y Grupo
+                    </h1>
+                    <Box className='flex flex-wrap justify-center gap-4'>
+                        <TagTitle tagGroup="A" />
+                        <TagTitle tagGroup="B" />
+                        <TagTitle tagGroup="C" />
+                        <TagTitle tagGroup="Total" />
+                    </Box>
+                </Box>
+
+                { /* Word Counters */ }
+
+                { /* Retrun Button */ }
+
+            </Box>
+        </PageMotionWrapper>
     )
 }
 
