a510ad3df04fe7280f90d03275c2b557336ede47
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-23 15:19:17 -0600

    Updated AnswersContainer component.
     - Added styles depending on prop send for incorrect and correct word container.

--- src/components/answersContainer/AnswersContainer.tsx
+++ src/components/answersContainer/AnswersContainer.tsx
@@ -1,5 +1,5 @@
-// import React from 'react'
-
+//import React from 'react'
+import { useState } from "react";
 import { Box } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import TagWord from "../tags/TagWord";
@@ -10,22 +10,23 @@ type AnswersContainerProps = {
 };
 
 function AnswersContainer({containerCorrect = false, wordsArray = []} : AnswersContainerProps ) {
+    const [words, setWords] = useState([...wordsArray]);
     const { t } = useTranslation();
 
-    // Add a function in the future or a prop function to allow add new words.
-    // Change the origin of words, use a useState([]) initialized with "wordsArray".
+    // Add a function in the future or a prop function to allow add new words with setWords.
 
     return (
-        <Box className='bg-purple-900 w-full h-[85%] p-1 rounded-xl border-secondary border-4 flex flex-col'>
+        <Box className='w-full h-[85%] p-1 rounded-xl border-secondary border-4 flex flex-col gap-1 bg-secondary-transparent'>
+
             {/* Answer Counter */}
-            <Box className='text-center text-3xl font-semibold'>
+            <Box className={`font-semibold text-center text-3xl ${containerCorrect ? 'text-green' : 'text-accent3'}`}>
                 <p> {containerCorrect ? t('components.answersContainer.correct') : t('components.answersContainer.incorrect')}: </p>
                 <p> 7 </p>
             </Box>
 
             {/* List of Answers */}
             <Box className='flex-1 flex flex-wrap content-start gap-1 overflow-auto'>
-                { wordsArray.map((word, index) => <TagWord key={index} word={word} tagStyleCorrect={containerCorrect} />) }
+                { words.map((word, index) => <TagWord key={index} word={word} tagStyleCorrect={containerCorrect} />) }
             </Box>
 
         </Box>
