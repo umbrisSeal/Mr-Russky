e0c3ec63ba515f348984b2498521e3828212b623
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-23 13:58:21 -0600

    Added note to TagWord component.

--- src/components/tags/TagWord.tsx
+++ src/components/tags/TagWord.tsx
@@ -6,7 +6,8 @@ import type { TagWordProps } from "./tagTypes";
 function TagWord({ tagStyleCorrect, word } :TagWordProps) {
     return (
         <Box className={`${tagStyleCorrect ? 'bg-dark-green border-green' : 'bg-accent2 border-accent3'} border-3 rounded-xl`}>
-            <p className="capitalize text-md text-accent font-semibold py-0.5 px-2">
+            {/* Note: Unsure if using text-sm or text-md. */}
+            <p className="capitalize text-sm text-accent font-semibold py-0.5 px-2">
                 {word}
             </p>
         </Box>
