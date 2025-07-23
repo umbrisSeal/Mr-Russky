7b0151e70f667c4778d76c83ffd6f06496144fd5
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-23 13:23:54 -0600

    Added new types for Tag components.

--- src/components/tags/tagTypes.ts
+++ src/components/tags/tagTypes.ts
@@ -3,6 +3,11 @@ export type TagProps = {
     tagGroup: "A" | "B" | "C" | "Total"
 };
 
+export type TagWordProps = {
+    word: string;
+    tagStyleCorrect: boolean;
+};
+
 
 export type TagCounterProps = TagProps & {
     counter: number;
