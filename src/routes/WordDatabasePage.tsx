318557f4f080c35f45faac59441793f23b86e50f
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-22 21:05:49 -0600

    Updated WordDatabasePage.
     - Added navigation to component.
     - Added dynamic size to allow component use the remaining window space.
     - Implemented the new MainPageButton component.

--- src/routes/WordDatabasePage.tsx
+++ src/routes/WordDatabasePage.tsx
@@ -4,13 +4,22 @@ import PageMotionWrapper from "../components/layout/PageMotionWrapper";
 import TagTitle from "../components/tags/TagTitle";
 import { useTranslation } from "react-i18next";
 import CategoriesContainer from "../components/wordCategoryList/CategoriesContainer";
+import { useNavigate } from "react-router";
+import MainPageButton from "../components/buttons/MainPageButton";
 
 function WordDatabasePage() {
     const { t } = useTranslation();
+    const navigateTo = useNavigate();
+
+    function handleReturnButton() {
+        navigateTo('/');
+    }
 
     return (
         <PageMotionWrapper>
-            <Box className=''>
+            {/* Change this in the future to use 100% of remaining space. */}
+            <Box className='flex flex-col [height:calc(100vh-60px)]'>
+
                 { /* Title and Tags */ }
                 <Box className='flex flex-col items-center gap-6 p-6'>
                     <h1 className="font-bold text-accent text-3xl text-center">
@@ -25,10 +34,14 @@ function WordDatabasePage() {
                 </Box>
 
                 { /* Word Counters */ }
-                <CategoriesContainer />
-                
+                <Box className='flex-1 overflow-auto p-4'>
+                    <CategoriesContainer />
+                </Box>
 
                 { /* Retrun Button */ }
+                <Box className='flex flex-col items-center p-4'>
+                    <MainPageButton buttonMessage={t('components.buttons.main.return')} onClickFn={handleReturnButton} />
+                </Box>
 
             </Box>
         </PageMotionWrapper>
