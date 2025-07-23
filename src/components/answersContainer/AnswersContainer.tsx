9894865247a1903a5b5581c3ba51b2a77cce01d9
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-23 14:47:00 -0600

    Added overflow control to AnswersContainer.

--- src/components/answersContainer/AnswersContainer.tsx
+++ src/components/answersContainer/AnswersContainer.tsx
@@ -4,13 +4,6 @@ import { Box } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import TagWord from "../tags/TagWord";
 
-// AnwsersContainer Props
-/*
-    Correct/Incorrect:
-    Array of initial words:
-    function to add a word?
-*/
-
 type AnswersContainerProps = {
     containerCorrect: boolean;      // True if it storages correct anwsers, false otherwise.
     wordsArray: string[];           // String of initial words shown in this component.
@@ -19,8 +12,11 @@ type AnswersContainerProps = {
 function AnswersContainer({containerCorrect = false, wordsArray = []} : AnswersContainerProps ) {
     const { t } = useTranslation();
 
+    // Add a function in the future or a prop function to allow add new words.
+    // Change the origin of words, use a useState([]) initialized with "wordsArray".
+
     return (
-        <Box className='bg-purple-900 w-full h-[85%] p-1 rounded-xl border-secondary border-4'>
+        <Box className='bg-purple-900 w-full h-[85%] p-1 rounded-xl border-secondary border-4 flex flex-col'>
             {/* Answer Counter */}
             <Box className='text-center text-3xl font-semibold'>
                 <p> {containerCorrect ? t('components.answersContainer.correct') : t('components.answersContainer.incorrect')}: </p>
@@ -28,7 +24,7 @@ function AnswersContainer({containerCorrect = false, wordsArray = []} : AnswersC
             </Box>
 
             {/* List of Answers */}
-            <Box className='flex flex-wrap gap-1'>
+            <Box className='flex-1 flex flex-wrap content-start gap-1 overflow-auto'>
                 { wordsArray.map((word, index) => <TagWord key={index} word={word} tagStyleCorrect={containerCorrect} />) }
             </Box>
 
--- src/components/tags/TagWord.tsx
+++ src/components/tags/TagWord.tsx
@@ -5,9 +5,9 @@ import type { TagWordProps } from "./tagTypes";
 
 function TagWord({ tagStyleCorrect, word } :TagWordProps) {
     return (
-        <Box className={`${tagStyleCorrect ? 'bg-dark-green border-green' : 'bg-accent2 border-accent3'} border-3 rounded-xl`}>
+        <Box className={`border-3 rounded-xl ${tagStyleCorrect ? 'bg-dark-green border-green' : 'bg-accent2 border-accent3'}`}>
             {/* Note: Unsure if using text-sm or text-md. */}
-            <p className="capitalize text-sm text-accent font-semibold py-0.5 px-2">
+            <p className="capitalize text-md text-accent font-semibold py-0.5 px-2">
                 {word}
             </p>
         </Box>
