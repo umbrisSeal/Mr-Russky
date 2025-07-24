702649f1a1e5e15f369086a84b0e76d3b546ac23
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-24 11:41:50 -0600

    Implemented LessonPanel in LessonPage.

--- src/routes/LessonPage.tsx
+++ src/routes/LessonPage.tsx
@@ -3,6 +3,7 @@
 import { Box } from "@mui/material";
 import PageMotionWrapper from "../components/layout/PageMotionWrapper";
 import AnswersContainer from "../components/answersContainer/AnswersContainer";
+import LessonPanel from "../components/lessonPanel/LessonPanel";
 
 function LessonPage() {
 
@@ -20,7 +21,12 @@ function LessonPage() {
 
                 {/* Central Secction */}
                 <Box className='bg-blue-700 p-2 flex-70 flex flex-col items-center'>
-                    <p> Central planel-secction here. </p>
+                    {/* Construir el panel aqui mismo. Usar boxes internas para cada elemento? Probablemente. */}
+                    <Box className='bg-blue-900 w-full flex-1'>
+                        <LessonPanel />
+                    </Box>
+                    <p className="bg-blue-800 w-full h-[120px]"> Progress bar and virtual keyboard. </p>
+                    <p className="bg-blue-950 w-full h-[60px]"> Exit button. </p>
                 </Box>
 
                 {/* Answer Container (Incorrect Words) */}
