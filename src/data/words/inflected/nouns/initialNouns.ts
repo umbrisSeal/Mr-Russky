898edfcfe4f59ab007213b92bdad904100fa1713
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-24 21:18:46 -0600

    Updated sample initialNouns to new word types.

--- src/data/words/inflected/nouns/initialNouns.ts
+++ src/data/words/inflected/nouns/initialNouns.ts
@@ -1,43 +1,16 @@
 
-
 // Initial noun words, only for example.
 
 import type { Noun } from "./noun";
 
+// А Б В Г Д Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Э Ю Я
+// Non included: Ь (soft), Ъ (hard).
 
 export const exampleNouns: Noun[] = [
-    {
-        id: "кошка",
-        gender: "female",
-        translation: {
-            es: "gata",
-            en: "cat"
-        },
-        cases: {
-            singular: {
-                nominative: "кошка",
-                genitive: "",
-                dative: "",
-                accusative: "",
-                instrumental: "",
-                prepositional: ""
-            },
-            plural: {
-                nominative: "кошки",
-                genitive: "",
-                dative: "",
-                accusative: "",
-                instrumental: "",
-                prepositional: ""
-            }
-        },
-        imageUrl: "",
-        difficulty: 1,
-        example: {
-            ru: "",
-            es: "",
-            en: ""
-        },
-        verified: false
+    { id: "кошка", gender: "female", translation: {es: "gata", en: "cat"},
+        cases: {singular: {nominative: "кошка", genitive: "", dative: "", accusative: "", instrumental: "", prepositional: ""},
+            plural: {nominative: "кошки", genitive: "", dative: "", accusative: "", instrumental: "", prepositional: ""}},
+        image: {imageUrl: "", imageAttributon: "", imageSource: ""},
+        difficulty: 1, group: 'A', categories: [], notes: "", example: {ru: "", es: "", en: ""}, verified: false, completed: false,
     }
 ];
