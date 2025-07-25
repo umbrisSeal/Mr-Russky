47d806f61bfaf3193864479d54a7e617a1bee6c6
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-24 20:55:14 -0600

    Added new data to baseWord types.

--- src/data/words/baseWord.ts
+++ src/data/words/baseWord.ts
@@ -12,14 +12,24 @@ export type Example = {
     en: string;     // Sentence in english using the english translation.
 };
 
+export type Image = {
+    imageUrl: string;           // URL of the image shown. For now, only from Wiki Commons.
+    imageAttributon: string;    // URL or information about author's attribution.
+    imageSource: string;        // Name of the source of the image. (Author, website, url, etc.)
+};
+
+export type Groups = 'A' | 'B' | 'C';
+
 
 export type BaseWord = {
     id: string;                 // Unique ID word. Word in russian.
     translation: Translation;   // Translations in spanish and english.
-    imageUrl: string;           // URL of the image shown. For now, only from Wiki Commons.
+    image: Image;               // URL of the image shown. For now, only from Wiki Commons.
+    group: Groups;              // A string value indicating its group.
+    categories: string[];       // An array of strings including the name of the ways to categorize a word.
     difficulty: number;         // Int number from 1 to 10. Used in future for difficulty level.
     example: Example;           // Example list using the word in russian, spanish and english sentences.
     verified: boolean;          // Indicates if translations are verified. Words not verified will not be shown in app lessons.
-    notes?: string;             // Optional notes, in english for this word.
+    notes: string;              // Optional notes, in english for this word.
     // transliteration          // For future phonetic sound.
 };
