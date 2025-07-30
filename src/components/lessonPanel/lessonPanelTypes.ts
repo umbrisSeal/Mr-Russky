adc3cf28d68c10dfc19e0f0d0971805864883734
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-07-30 16:28:27 -0600

    Added new types for LessonPanelProps.

--- src/components/lessonPanel/lessonPanelTypes.ts
+++ src/components/lessonPanel/lessonPanelTypes.ts
@@ -3,7 +3,8 @@ import type { Noun } from "../../data/words/inflected/nouns/noun";
 export type LessonPanelProps = {
     wordType: string;
     currentExcerciceIndex: number;
+    showingAnswer: boolean;             // Indicates if LessonPanel should show the correct answer.
     hintButtonFn: () => void;
-    okButtonFn: () => void;
+    okButtonFn: (userAnswer: string) => void;
     lessonVocabulary: Noun[];           // Must change in the future, not only Nouns are used.
 }
