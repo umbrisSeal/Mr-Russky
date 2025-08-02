01ff35e3026f743ed1afe6bacd91c8a75ef2c202
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-08-02 12:36:05 -0600

    Deleted unnecesary import in lessonLoader.

--- src/routes/loaders/lessonLoader.ts
+++ src/routes/loaders/lessonLoader.ts
@@ -3,20 +3,19 @@
     Also, the result (or the filter) should be added as a cookie in the app to allow user to repeat this lesson.
 */
 
-import { useNavigate, type LoaderFunctionArgs } from "react-router";
+import { type LoaderFunctionArgs } from "react-router";
 import type { Noun } from "../../data/words/inflected/nouns/noun";
 import { exampleNouns } from "../../data/words/inflected/nouns/initialNouns";
 import { shuffleVocabulary } from "../../utils/functions/shuffleVocabulary";
 
 
 export async function lessonLoader({request} : LoaderFunctionArgs ) : Promise<Noun[]> {
-    const navigateTo = useNavigate();
     const url = new URL(request.url);
     
     // Temporal params:
     const studyAll = url.searchParams.get("studyAll");
-
-    if(!studyAll) navigateTo('/error'); // Wrong params recieved.
+    
+    if(!studyAll) return []; // Wrong params recieved.
 
     const appVocabulary : Noun[] = exampleNouns;
     let lessonVocabulary = [];
