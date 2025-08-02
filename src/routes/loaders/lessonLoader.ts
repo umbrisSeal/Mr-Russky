a015cd0b2d6fa27603b354f9da627acb41e47ad5
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-08-02 12:26:47 -0600

    Updated loader to load vocabulary to study.

--- src/routes/loaders/lessonLoader.ts
+++ src/routes/loaders/lessonLoader.ts
@@ -1,4 +1,36 @@
+/*
+    This loader filters the vocabulary of all the app and presents the specific vocabulary for a particular lesson.
+    Also, the result (or the filter) should be added as a cookie in the app to allow user to repeat this lesson.
+*/
 
-export async function lessonLoader() {
-    return;
+import { useNavigate, type LoaderFunctionArgs } from "react-router";
+import type { Noun } from "../../data/words/inflected/nouns/noun";
+import { exampleNouns } from "../../data/words/inflected/nouns/initialNouns";
+import { shuffleVocabulary } from "../../utils/functions/shuffleVocabulary";
+
+
+export async function lessonLoader({request} : LoaderFunctionArgs ) : Promise<Noun[]> {
+    const navigateTo = useNavigate();
+
+    const url = new URL(request.url);
+    
+    // Temporal params:
+    const studyAll = url.searchParams.get("studyAll");
+
+    console.log(studyAll);
+
+    if(!studyAll) navigateTo('/error'); // Wrong params recieved.
+
+    const appVocabulary : Noun[] = exampleNouns;
+    let vocabularyToStudy = [];
+
+    if(studyAll === '1') {
+        // Study all vocabulary.
+        vocabularyToStudy = shuffleVocabulary(appVocabulary);
+    } else {
+        // Only study 10 random words.
+        vocabularyToStudy = shuffleVocabulary(appVocabulary.slice(0, 10));
+    }
+
+    return vocabularyToStudy;
 };
