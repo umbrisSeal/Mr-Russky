c87beed276cdf79deb14895ecbc7640115d26b2a
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-08-02 16:12:22 -0600

    Added lessonResults hook to keep track of user results.

---
+++ src/hooks/lessonResults.ts
@@ -0,0 +1,29 @@
+
+
+
+/*
+    wrongAnswersArray           -> Keep for calculations
+    correctAnswersArray         -> Keep for calculations
+*/
+
+
+import { create } from "zustand"
+import type { Noun } from "../data/words/inflected/nouns/noun";
+
+interface ResultsStore {
+    wrongAnswers: Noun[],
+    correctAnswers: Noun[],
+    addWrongAnswer: (word: Noun) => void;
+    addCorrectAnswer: (word: Noun) => void;
+};
+
+export const useLessonResultsStore = create<ResultsStore>((set) => ({
+    wrongAnswers: [],
+    correctAnswers: [],
+    addWrongAnswer: (word) => set((state) => ({
+        wrongAnswers: [...state.wrongAnswers, word],
+    })),
+    addCorrectAnswer: (word) => set((state) => ({
+        correctAnswers: [...state.correctAnswers, word],
+    }))
+}));
