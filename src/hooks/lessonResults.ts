adbebc9efe4939d8e3cd8356a867a99954c69d2e
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-08-02 16:39:38 -0600

    Updated lessonResults hook to allow reset results.

--- src/hooks/lessonResults.ts
+++ src/hooks/lessonResults.ts
@@ -5,17 +5,28 @@ import type { Noun } from "../data/words/inflected/nouns/noun";
 interface ResultsStore {
     wrongAnswers: Noun[],
     correctAnswers: Noun[],
+    previousCorrectAnswers: Noun[],
+    previousWrongAnswers: Noun[],
     addWrongAnswer: (word: Noun) => void;
     addCorrectAnswer: (word: Noun) => void;
+    resetResults: () => void;
 };
 
 export const useLessonResultsStore = create<ResultsStore>((set) => ({
     wrongAnswers: [],
     correctAnswers: [],
+    previousWrongAnswers: [],
+    previousCorrectAnswers: [],
     addWrongAnswer: (word) => set((state) => ({
         wrongAnswers: [...state.wrongAnswers, word],
     })),
     addCorrectAnswer: (word) => set((state) => ({
         correctAnswers: [...state.correctAnswers, word],
+    })),
+    resetResults: () => set((state) => ({
+        previousWrongAnswers: [...state.wrongAnswers],
+        previousCorrectAnswers: [...state.correctAnswers],
+        wrongAnswers: [],
+        correctAnswers: [],
     }))
 }));
