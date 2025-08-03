ca928b4d558f9b5769b596d9e2767023fb2cd934
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2025-08-02 21:23:22 -0600

    Updated ResultsPage to show the real results.

--- src/routes/ResultsPage.tsx
+++ src/routes/ResultsPage.tsx
@@ -5,9 +5,34 @@ import SecondaryOptionButton from '../components/buttons/SecondaryOptionButton';
 import TertiaryOptionButton from '../components/buttons/TertiaryOptionButton';
 import PageMotionWrapper from '../components/layout/PageMotionWrapper';
 import { Box } from '@mui/material';
+import { useLessonResultsStore } from '../hooks/lessonResults';
+import { useNavigate } from 'react-router';
+
 
 function ResultsPage() {
+    const wrongAnswers = useLessonResultsStore((state) => state.wrongAnswers);
+    const correctAnswers = useLessonResultsStore((state) => state.correctAnswers);
+    const previousWrongAnswers = useLessonResultsStore((state) => state.previousWrongAnswers);
+    const previousCorrectAnswers = useLessonResultsStore((state) => state.previousCorrectAnswers);
     const { t } = useTranslation();
+    const navigateTo = useNavigate();
+
+    const totalWords = wrongAnswers.length + correctAnswers.length;
+    const vocabularyMastery = (correctAnswers.length / totalWords) * 100;
+
+
+    function handleRepeatLesson() {
+        navigateTo('/lesson?repeat=1');
+    };
+
+    function handleRepeatOnlyErrors() {
+        navigateTo('/lesson?errors=1');
+    };
+
+    function handleGoHome() {
+        navigateTo('/');
+    };
+
 
     return(
         <PageMotionWrapper>
@@ -24,11 +49,11 @@ function ResultsPage() {
                             flex items-center justify-around'
                     >
                         <Box className='text-green'>
-                            <p className='font-bold text-5xl'> 58 </p>
+                            <p className='font-bold text-5xl'> {correctAnswers.length} </p>
                             <p className='text-xl'> {t('pages.results.correctWords')} </p>
                         </Box>
                         <Box className='text-accent3'>
-                            <p className='font-bold text-5xl'> 12 </p>
+                            <p className='font-bold text-5xl'> {wrongAnswers.length} </p>
                             <p className='text-xl'> {t('pages.results.incorrectWords')} </p>
                         </Box>
                         <Box className='text-secondary'>
@@ -47,14 +72,14 @@ function ResultsPage() {
                             flex items-center justify-center'
                         >
                             <Box className=''>
-                                <p className='font-bold text-5xl'> 70 </p>
+                                <p className='font-bold text-5xl'> {totalWords} </p>
                                 <p className='text-xl'> {t('pages.results.totalWords')} </p>
                             </Box>
                         </Box>
 
                         <Box className=' text-accent pr-8 flex pt-4 justify-around items-center gap-8'>
                             <p className='text-xl'> {t('pages.results.vocabularyMastery')} </p>
-                            <p className='text-7xl font-bold'> 87.62 % </p>
+                            <p className='text-7xl font-bold'> {`${vocabularyMastery.toFixed(2)} %`} </p>
                         </Box>
                     </Box>
 
@@ -68,9 +93,9 @@ function ResultsPage() {
 
                     {/* Buttons */}
                     <Box className='flex flex-col gap-2'>
-                        <PrimaryOptionButton buttonMessage={t('components.buttons.options.repeatLesson')} />
-                        <TertiaryOptionButton buttonMessage={t('components.buttons.options.practiceErrors')} />
-                        <SecondaryOptionButton buttonMessage={t('components.buttons.options.returnHome')} />
+                        <PrimaryOptionButton buttonMessage={t('components.buttons.options.repeatLesson')} onClickFn={handleRepeatLesson} />
+                        <TertiaryOptionButton buttonMessage={t('components.buttons.options.practiceErrors')} onClickFn={handleRepeatOnlyErrors} />
+                        <SecondaryOptionButton buttonMessage={t('components.buttons.options.returnHome')} onClickFn={handleGoHome} />
                     </Box>
                 </Box>
 
